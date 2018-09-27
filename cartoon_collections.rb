3db9498b2d99d042151c230da1610091696a6a20
rosa lee <rosalee0116@gmail.com>
2018-09-27 23:41:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,11 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)# code an argument here
+  short_words.each do |call|# Your code here
+    return true if call.length > 4
+end
+false
 end
 
 def find_the_cheese# code an argument here
